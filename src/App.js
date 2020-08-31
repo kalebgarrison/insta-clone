50a8728a79ce6ff0855962ff2b305dad1269d030
kalebgarrison <kalebgarrison@gmail.com>
2020-08-31 11:55:08 -0400

    added starter code and added header image and post component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="app__header">
+        <img
+        className="app__headerImage"
+        src="https://www.logo.wine/a/logo/Instagram/Instagram-Wordmark-Logo.wine.svg"
+        alt=""
+        />
+      </div>
+      <h1>Hello Everyone!</h1>
+
+      {/* Header */}
+
+      {/* Posts */}
     </div>
   );
 }

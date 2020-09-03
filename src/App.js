449a8ca0847eaf89aeb3e690ec80903d8db408f6
kalebgarrison <kalebgarrison@gmail.com>
2020-09-03 11:29:59 -0400

    added image upload function. Also, added styling to image upload div

--- src/App.js
+++ src/App.js
@@ -1,51 +1,187 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./components/Post/Post";
-import { db } from "./firebase"
+import { db, auth } from "./firebase";
+// import classes from "*.module.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+import ImageUpload from "./components/ImageUpload/ImageUpload";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   username: "kaptainklob77",
-    //   caption: "insta clone!!",
-    //   imageUrl: "https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg"
-    // },
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
 
-    // {
-    //   username: "kaptainklob77",
-    //   caption: "insta clone!!",
-    //   imageUrl: "https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg"
-    // },
-
-
-  ]);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()))
-    })
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
 
-  }, [])
+    return () => {
+      unsubscribe();
+    };
+  }, [user, username]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() }))
+        );
+      });
+  }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  };
 
   return (
     <div className="App">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img
+              className="app__headerImage"
+              src="https://www.logo.wine/a/logo/Instagram/Instagram-Wordmark-Logo.wine.svg"
+              alt=""
+            />
+          </center>
+          <form className="app__signup">
+            <Input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+      <Modal open={openSignIn} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img
+              className="app__headerImage"
+              src="https://www.logo.wine/a/logo/Instagram/Instagram-Wordmark-Logo.wine.svg"
+              alt=""
+            />
+          </center>
+          <form className="app__signup">
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.logo.wine/a/logo/Instagram/Instagram-Wordmark-Logo.wine.svg"
           alt=""
         />
+        {user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
-      <h1>Hello Everyone!</h1>
-
-      {/* Header */}
 
       {/* Posts */}
-      {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-        ))
-      }
+
+      <div className="app__posts">
+        {posts.map(({ id, post }) => (
+          <Post
+            key={id}
+            username={post.username}
+            caption={post.caption}
+            imageUrl={post.imageUrl}
+          />
+        ))}
+      </div>
+
       {/* <Post
         imageUrl="https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg"
         username="kaptainklob77"
@@ -61,6 +197,12 @@ function App() {
         caption="I'm lost"
         imageUrl="https://bostonglobe-prod.cdn.arcpublishing.com/resizer/qVY4TW15GGYpX68SDdhQWczGd1E=/1440x0/cloudfront-us-east-1.images.arcpublishing.com/bostonglobe/RX7S4QMMIPUC6FPGJPBKRH5RQI.jpg"
       /> */}
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Sorry you need to login to upload</h3>
+      )}
     </div>
   );
 }
